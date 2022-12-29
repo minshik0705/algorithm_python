c317123a899e4e7c843879c5e40722bd55261fa3
Minshik <jmyang99@naver.com>
2022-12-29 16:04:18 +0900

    알고리즘 5일차

--- section2/problem1_primary_of_K.py
+++ section2/problem1_primary_of_K.py
@@ -1,3 +1,4 @@
+'''
 import sys
 #sys.stdin=open("input.txt", "rt")
 n, k=map(int, input().split())
@@ -10,3 +11,16 @@ for i in range(1, n+1):
         break
 else:
     print(-1)
+'''
+#내 풀이 
+n,k=map(int, input().split())
+a=[]
+for i in range (1,n+1):
+    if n%i==0:
+        a.append(i)
+size=len(a)
+if size>=k:
+    print(a[k-1])
+else:
+    print(-1)
+
