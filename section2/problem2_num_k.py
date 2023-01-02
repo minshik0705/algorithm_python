ecca5332c58a20e68ccace12af90226b99f3d049
Minshik <jmyang99@naver.com>
2023-01-02 23:07:18 +0900

    알고리즘8일차

--- section2/problem2_num_k.py
+++ section2/problem2_num_k.py
@@ -1,12 +1,16 @@
 import sys
 #sys.stdin=open("input.txt", "rt")
-n, k=map(int, input().split())
-cnt=0
-for i in range(1, n+1):
-    if n%i==0:
-        cnt+=1
-    if cnt==k:
-        print(i)
-        break
-else:
-    print(-1)
+T=int(input())
+for t in range(T):
+    n, s, e, k=map(int, input().split())
+    a=list(map(int, input().split()))
+    a=a[s-1:e]
+    a.sort()
+    print("#%d %d" %(t+1, a[k-1]))
+    
+'''
+map(함수, 반복가능한 자료형(리스트, 튜플)) function, iterables라고 
+map함수의 반환값은 map객체이기 때문에 해당 자료형을 list또는 tuple형으로 변환해야 한다.
+리스트 또는 튜플의 각 요소에 접근할때 용이하게 사용된다.
+
+      
