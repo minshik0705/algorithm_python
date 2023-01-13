2ad46542829f4d8d68ac8b2deb64eb190a38ab8d
Minshik <jmyang99@naver.com>
2023-01-13 23:20:03 +0900

    알고리즘15일차

--- section2/problem9_dice_game.py
+++ section2/problem9_dice_game.py
@@ -1,5 +1,5 @@
 '''
-주사위 게임
+9.주사위 게임
 '''
 import sys
 #sys.stdin=open("input.txt", "rt")
@@ -19,6 +19,31 @@ for i in range(n):
         money=c*100
     if money>res:
         res=money
-
 print(res)
+
+#내풀이 
+'''
+N=int(input())
+res=[0]*N
+#리스트의 선언을 for문 안에서 하면 리스트가 계속 초기화된다.
+money=0
+for i in range(N):
+    a,b,c=map(int, input().split())
+    k=list()
+    k.append(a)
+    k.append(b)
+    k.append(c)
+    k.sort()
+    if a==b and b==c:
+        money=10000+a*1000
+    elif a==b:
+        money=1000+a*100
+    elif b==c:
+        money=1000+b*100
+    else:
+        money=100*a
+    res[i]=money
+res.sort(reverse=True)
+print(res[0])
+'''
 
